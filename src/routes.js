c40e21d5a9a75b915338a01f857cabae81141167
llicar <lucas.licar.sousa@gmail.com>
2021-04-17 22:10:13 -0300

    Adiciona middleware auth as rotas

--- src/routes.js
+++ src/routes.js
@@ -2,28 +2,30 @@
 import Router from 'express';
 import JovensController from './app/controllers/JovensController.js';
 import EmpresasController from './app/controllers/EmpresasController.js';
+import LoginController from './app/controllers/LoginController.js';
 import multer from 'multer';
 
+import auth from './app/middlewares/auth.js'
 import multerConfig from './config/upload.js';
 
 const routes = Router();
 const upload = multer(multerConfig);
 
-routes.get('/jovens', JovensController.index);
-routes.get('/jovem/:id', JovensController.show);
-routes.get('/anotacoes/:id', JovensController.showNotes);
-routes.get('/jovensEmProcesso', JovensController.buscarJovensEmProcesso);
+routes.post('/login', LoginController.login);
 
-routes.post('/jovens', JovensController.create);
-routes.post('/update1/:id', upload.single('calendario'), JovensController.update1);
-routes.post('/update2/:id', JovensController.update2);
+routes.get('/jovens', auth, JovensController.index);
+routes.get('/jovem/:id', auth, JovensController.show);
+routes.get('/anotacoes/:id', auth, JovensController.showNotes);
+routes.get('/jovensEmProcesso', auth, JovensController.buscarJovensEmProcesso);
 
-routes.post('/anotacoes/:id', JovensController.createNote);
-routes.post('/updateEtapa/:id', JovensController.updateEtapa);
+routes.post('/jovens', auth, JovensController.create);
+routes.post('/update1/:id', auth, upload.single('calendario'), JovensController.update1);
+routes.post('/update2/:id', auth, JovensController.update2);
 
+routes.post('/anotacoes/:id', auth, JovensController.createNote);
+routes.post('/updateEtapa/:id', auth, JovensController.updateEtapa);
 
-
-routes.get('/empresas', EmpresasController.index);
-routes.post('/empresas', EmpresasController.create);
+routes.get('/empresas', auth, EmpresasController.index);
+routes.post('/empresas', auth, EmpresasController.create);
 
 export default routes;
