1a9dfb535cd85b569bcdab4d4e5dce8bb5adce80
llicar <lucas.licar.sousa@gmail.com>
2021-04-17 23:09:42 -0300

    remove host

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import { app } from './app.js';
 
 const port = process.env.PORT ? process.env.PORT : 3001;
-const host = '0.0.0.0';
 
 app.listen(port, host, () => {
   console.log(`***** SERVER STARTED ON PORT ${port} *****`);
