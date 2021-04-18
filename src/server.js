f4f54687468a92ba38dc53c489d54a3abf5f8c71
llicar <lucas.licar.sousa@gmail.com>
2021-04-17 23:01:48 -0300

    add host

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import { app } from './app.js';
 
 const port = process.env.PORT ? process.env.PORT : 3001;
+const host = '0.0.0.0';
 
-app.listen(port, () => {
+app.listen(port, host, () => {
   console.log(`***** SERVER STARTED ON PORT ${port} *****`);
 });
