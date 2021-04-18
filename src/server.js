739d9f691cc960213fc7c88a314d6b218a883030
llicar <lucas.licar.sousa@gmail.com>
2021-04-18 16:32:34 -0300

    update port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import { app } from './app.js';
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`***** SERVER STARTED ON PORT ${port} *****`);
