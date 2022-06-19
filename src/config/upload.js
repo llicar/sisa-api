ef7d5120d95992a84b21c47c00fbfefcc663c48a
llicar <lucas.licar.sousa@gmail.com>
2022-06-19 20:54:13 -0300

    inclui upload de atestado no multer

--- src/config/upload.js
+++ src/config/upload.js
@@ -8,38 +8,50 @@ import md5 from 'md5';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const storageTypes ={
-    
-    local: multer.diskStorage({
-        destination: path.join(__dirname,'..','..','uploads'),
-        filename:(request,file,cb) =>{
+const storageTypes = {
 
-                const {id} = request.params;
-                const type = file.originalname.split(".");
-                const md5Name = md5(file.originalname);
-                file.key = `${id}-${md5Name}.${type[1]}`;
-                cb(null,file.key);
+    local: multer.diskStorage({
+        destination: path.join(__dirname, '..', '..', 'uploads'),
+        filename: (request, file, cb) => {
+
+            const { id } = request.params;
+            const type = file.originalname.split(".").pop();
+            const md5Name = md5(file.originalname);
+            file.key = `${id}-${md5Name}.${type[1]}`;
+            cb(null, file.key);
         }
     }),
 
-    s3: multerS3({
+    s3Calendario: multerS3({
         s3: new aws.S3(),
         bucket: 'scja/calendarios',
         contentType: multerS3.AUTO_CONTENT_TYPE,
-        acl:'public-read',
-        key: (request,file,cb) =>{
-                const {id} = request.params;
-                const type = file.originalname.split(".");
-                const md5Name = md5(file.originalname);
-                const filename = `${id}-${md5Name}.${type[1]}`;
-                cb(null,filename);
+        acl: 'public-read',
+        key: (request, file, cb) => {
+            const { id } = request.params;
+            const type = file.originalname.split(".").pop();
+            const md5Name = md5(file.originalname);
+            const filename = `${id}-${md5Name}.${type}`;
+            cb(null, filename);
+        }
+    }),
+
+    s3Atestado: multerS3({
+        s3: new aws.S3(),
+        bucket: 'scja/atestados',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        key: (request, file, cb) => {
+            const { id } = request.params;
+            const type = file.originalname.split(".").pop();
+            const md5Name = md5(file.originalname);
+            const filename = `${id}-${md5Name}.${type}`;
+            cb(null, filename);
         }
     })
 }
 
-const multerConfig = {
-    storage: storageTypes["s3"]
-}
+export const multerConfigCalendario = { storage: storageTypes["s3Calendario"] }
+export const multerConfigAtestado = { storage: storageTypes["s3Atestado"] }
 
 
-export default multerConfig;
