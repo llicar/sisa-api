8539fd1d4ac06e57b9f29dc15a535f70f1896093
llicar <lucas.licar.sousa@gmail.com>
2022-06-19 21:15:26 -0300

    :sparkles: feat: adicionando rotas de faltas e modalidades

--- src/routes.js
+++ src/routes.js
@@ -3,25 +3,32 @@ import Router from 'express';
 import JovensController from './app/controllers/JovensController.js';
 import EmpresasController from './app/controllers/EmpresasController.js';
 import LoginController from './app/controllers/LoginController.js';
+import ModalidadeController from './app/controllers/ModalidadeController.js';
+import FaltasController from './app/controllers/FaltasController.js';
+
 import multer from 'multer';
 
 import auth from './app/middlewares/auth.js'
-import multerConfig from './config/upload.js';
+import { multerConfigCalendario, multerConfigAtestado } from './config/upload.js';
 
 const routes = Router();
-const upload = multer(multerConfig);
+const uploadCalendario = multer(multerConfigCalendario);
+const uploadAtestado = multer(multerConfigAtestado);
 
+//Rota de login
 routes.post('/login', LoginController.login);
 
-routes.get('/jovens', auth, JovensController.index);
-routes.get('/inventario', auth, JovensController.inventario);
-routes.get('/jovem/:id', auth, JovensController.show);
-routes.get('/anotacoes/:id', auth, JovensController.showNotes);
-routes.get('/jovensEmProcesso', auth, JovensController.buscarJovensEmProcesso);
-routes.get('/jovensPorEmpresa/:idEmpresa', auth, JovensController.buscarJovensPorEmpresa);
+//Rotas da entidade Jovem
+routes.get('/jovens', auth, JovensController.index); //Buscar todos os jovems
+routes.get('/inventario', auth, JovensController.inventario); // Buscar Inventário de jovens
+routes.get('/jovem/:id', auth, JovensController.show); // Buscar jovem por id
+routes.post('/jovens', auth, JovensController.create); // Cadastrar jovem
+routes.get('/jovensEmProcesso', auth, JovensController.buscarJovensEmProcesso); // Buscar jovens em precesso
+routes.get('/jovensPorEmpresa/:idEmpresa', auth, JovensController.buscarJovensPorEmpresa); // Buscar jovens por empresa
 
-routes.post('/jovens', auth, JovensController.create);
-routes.post('/update1/:id', auth, upload.single('calendario'), JovensController.update1);
+
+routes.get('/anotacoes/:id', auth, JovensController.showNotes);
+routes.post('/update1/:id', auth, uploadCalendario.single('calendario'), JovensController.update1);
 routes.post('/update2/:id', auth, JovensController.update2);
 
 routes.post('/anotacoes/:id', auth, JovensController.createNote);
@@ -30,10 +37,23 @@ routes.post('/finalizarAdmissao/:id', auth, JovensController.finalizarAdmissao);
 routes.get('/empresas', auth, EmpresasController.index);
 routes.get('/empresas/:id', auth, EmpresasController.BuscarEmpresaPorId);
 routes.post('/empresas', auth, EmpresasController.create);
-
-
+routes.post('/empresas/:id', auth, EmpresasController.alterarEmpresaPorId);
+routes.get('/invEmpresas', auth, EmpresasController.InvEmpresas);
 
 routes.post('/deletarAdmissao/:id', auth, JovensController.deletarAdmissao);
 routes.post('/desligarJovem/:id', auth, JovensController.desligarJovem);
+
+routes.post('/faltas/:id', auth, uploadAtestado.single('atestado'), FaltasController.create);
+routes.post('/deletarFalta/:id', auth, FaltasController.deletarFalta);
+routes.post('/atualizarFalta/:id', auth, uploadAtestado.single('atestado'), FaltasController.atualizarFalta);
+
+routes.get('/faltas', auth, FaltasController.index);
+routes.get('/faltas/:id', auth, FaltasController.buscarFaltaPorId);
+
+routes.get('/modalidade', auth, ModalidadeController.index);
+
+
+
+
 
 export default routes;
